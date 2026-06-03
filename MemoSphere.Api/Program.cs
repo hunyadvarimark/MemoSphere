d7cbc744b05edb9c19936ee9017a5902a7d45ba3
hmark0921 <hmark0921@gmail.com>
2026-06-03 14:29:58 +0200

    api init

---
+++ MemoSphere.Api/Program.cs
@@ -0,0 +1,32 @@
+
+namespace MemoSphere.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            builder.Services.AddOpenApi();
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
